ba15f19a1756e3120550c04de7b91372b2306626
ankit singh tanwar <ankitsingh.ktp2001@gmail.com>
2025-03-18 11:35:41 +0530

    assignment 1 added

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+class student
+{
+
+    public string name;
+    public int RollNumber;
+    public string course;
+    public int Age;
+
+    public student(string name, int RollNumber, string course, int Age)
+    {
+        this.name = name;
+        this.RollNumber = RollNumber;
+        this.course = course;
+        this.Age = Age;
+    }
+
+    public void showDetails()
+    {
+        Console.WriteLine("Name : " + this.name);
+        Console.WriteLine("Roll Number :" + this.RollNumber);
+        Console.WriteLine("course :" + this.course);
+        Console.WriteLine("Age :" + this.Age);
+
+        
+
+
+    }
+
+
+}
+
+class main
+{
+    public static void Main()
+    {
+        student s1 = new student("abhishek", 1, "web Development", 20);
+        student s2 = new student("rahul", 2, "UI Designing", 21);
+        student s3 = new student("ajay", 3, "Machine Learning", 20);
+
+        s1.showDetails();
+        s2.showDetails();
+        s3.showDetails();
+    }
+}
+
+ 
