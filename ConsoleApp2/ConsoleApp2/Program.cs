9104b97ca49ea50807d6f4ef4a7e8ac3bb8035f8
ankit singh tanwar <ankitsingh.ktp2001@gmail.com>
2025-03-22 19:49:23 +0530

    assigment2add

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -7,49 +7,65 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-class student
+internal interface ILogger
 {
-
-    public string name;
-    public int RollNumber;
-    public string course;
-    public int Age;
-
-    public student(string name, int RollNumber, string course, int Age)
-    {
-        this.name = name;
-        this.RollNumber = RollNumber;
-        this.course = course;
-        this.Age = Age;
-    }
-
-    public void showDetails()
-    {
-        Console.WriteLine("Name : " + this.name);
-        Console.WriteLine("Roll Number :" + this.RollNumber);
-        Console.WriteLine("course :" + this.course);
-        Console.WriteLine("Age :" + this.Age);
-
-        
-
-
-    }
-
-
+    void Log();
 }
 
+public class DatabaseLogger : ILogger
+{
+
+    public void Log()
+    {
+        Console.WriteLine("you are successfully Loged in Data base");
+    }
+}
+public class FileLogger : ILogger
+{
+
+    public void Log()
+    {
+        Console.WriteLine("you are successfully Loged in File");
+    }
+}
+public class CloudLogger : ILogger
+{
+    public void Log()
+    {
+        Console.WriteLine(" you are successfully Loged in Cloud");
+    }
+}
 class main
 {
     public static void Main()
     {
-        student s1 = new student("abhishek", 1, "web Development", 20);
-        student s2 = new student("rahul", 2, "UI Designing", 21);
-        student s3 = new student("ajay", 3, "Machine Learning", 20);
-
-        s1.showDetails();
-        s2.showDetails();
-        s3.showDetails();
+        main ma = new main();
+        ILogger Logs;
+        Console.WriteLine("Enter the Log Type (Database, File ,Cloud);");
+        string LogType = Console.ReadLine();
+        Logs = ma.getLogger(LogType);
+        Logs.Log();
+       
+    }
+    public ILogger getLogger(string type)
+    {
+        ILogger Logs;
+        if(type == "Database")
+        {
+            Logs = new DatabaseLogger();
+        }
+        else if(type == "File")
+        {
+            Logs = new FileLogger();
+        }
+        else
+        {
+            Console.WriteLine("The log type is not exist");
+            return null;
+        }
+        return Logs;
     }
 }
 
- 
+
+
