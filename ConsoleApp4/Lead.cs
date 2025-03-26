826feb92890a2cbc4545e024905f5a8e66fe6925
Ankitsinghtanwar23 <ankitsingh.ktp2001@gmail.com>
2025-03-26 19:52:25 +0530

    Add files via upload

---
+++ ConsoleApp4/Lead.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Assignment3
+{
+    internal class Lead
+    {
+        internal static void GeneralEnquiry()
+        {
+            throw new ImplementedException();
+        }
+
+        public static implicit operator Lead(OrderProcessor.Leads v)
+        {
+            throw new ImplementedException();
+        }
+    }
+}
